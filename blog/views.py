57e52c6e1725361ad3b9e7c4fef033aac1ab9619
Karla G. <k.grajalesluna@gmail.com>
2024-03-26 16:24:10 +0000

    adding blog/templates and blog/urls.py updated blog mysite

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+#def is't a function called post_list that takes request and will return 
+# the value it gests from calling another function render that will render 
+# (put toguether) our template blogs/post_list.html
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
