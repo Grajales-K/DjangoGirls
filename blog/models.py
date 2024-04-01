88eba1d694341b8876643fef7843fd312e811a79
Karla G. <k.grajalesluna@gmail.com>
2024-04-01 10:24:23 +0100

    adding new Post, modifiying git.ignore

--- blog/models.py
+++ blog/models.py
@@ -19,3 +19,15 @@ class Post(models.Model):
         
     def __str__(self):
         return self.title
+
+
+>>> from blog.models import Post
+>>> Post.objects.all()
+<QuerySet [<Post: Resume-Building Workshop>, <Post: Backend Study Group [Online]: Designing Data-Intensive Applications (Chapter 4)>, <Post: learning python>]>
+>>> 
+
+
+➜  djangogirls git:(main) git status
+On branch main
+nothing to commit, working tree clean
+➜  djangogirls git:(main) 
