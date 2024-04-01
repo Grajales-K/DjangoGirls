4e2712a1c13e40b0aabe60db4de3cfc86d751c0e
Karla G. <k.grajalesluna@gmail.com>
2024-04-01 10:43:52 +0100

    make correction on .gitignore deleting unneeded files

--- blog/models.py
+++ blog/models.py
@@ -19,15 +19,3 @@ class Post(models.Model):
         
     def __str__(self):
         return self.title
-
-
->>> from blog.models import Post
->>> Post.objects.all()
-<QuerySet [<Post: Resume-Building Workshop>, <Post: Backend Study Group [Online]: Designing Data-Intensive Applications (Chapter 4)>, <Post: learning python>]>
->>> 
-
-
-➜  djangogirls git:(main) git status
-On branch main
-nothing to commit, working tree clean
-➜  djangogirls git:(main) 
